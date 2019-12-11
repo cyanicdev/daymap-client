d297631b1c8d411dc4c7eee601d941160c1d794f
mitchpk <30470175+mitchpk@users.noreply.github.com>
2019-12-11 21:12:36 +1030

    FINISHED timetable

--- scripts/common.js
+++ scripts/common.js
@@ -18,6 +18,10 @@ document.onreadystatechange = () => {
     }
 };
 
+String.prototype.toSentenceCase = function () {
+    return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+};
+
 function handleWindowControls() {
     if(process.platform == "win32") {
         // Windows
--- scripts/timetable.js
+++ scripts/timetable.js
@@ -1,18 +1,87 @@
 function createTimetable(classes) {
     // param classes: array of classes with information such as room, teacher, time etc.
-    // this function will calculate length of classes and create elements in the timetable tab accordingly
+    
     Array.from(document.getElementsByClassName('line1')).forEach(e => {
-        e.childNodes[1].innerText = classes.childNodes[4].childNodes[1].getElementsByClassName('ttSubject')[0].innerText;
-        e.childNodes[5].innerText = classes.childNodes[4].childNodes[1].getElementsByClassName('ttRoom')[0].innerText.split('\n')[0];
-        e.childNodes[3].innerText = classes.childNodes[4].childNodes[1].getElementsByClassName('ttRoom')[0].innerText.split('\n')[1];
+        if(classes[0][0]) {
+            e.children[0].innerText = classes[0][0].subject;
+            e.children[1].innerText = classes[0][0].teacher;
+            e.children[2].innerText = classes[0][0].room;
+        } else {
+            e.children[0].innerText = "Free";
+        }
     });
 
     Array.from(document.getElementsByClassName('line2')).forEach(e => {
-        e.childNodes[1].innerText = classes.childNodes[5].childNodes[1].getElementsByClassName('ttSubject')[0].innerText;
-        e.childNodes[5].innerText = classes.childNodes[5].childNodes[1].getElementsByClassName('ttRoom')[0].innerText.split('\n')[0];
-        e.childNodes[3].innerText = classes.childNodes[5].childNodes[1].getElementsByClassName('ttRoom')[0].innerText.split('\n')[1];
+        if(classes[0][1]) {
+            e.children[0].innerText = classes[0][1].subject;
+            e.children[1].innerText = classes[0][1].teacher;
+            e.children[2].innerText = classes[0][1].room;
+        } else {
+            e.children[0].innerText = "Free";
+        }
+    });
+
+    Array.from(document.getElementsByClassName('line3')).forEach(e => {
+        if(classes[0][2]) {
+            e.children[0].innerText = classes[0][2].subject;
+            e.children[1].innerText = classes[0][2].teacher;
+            e.children[2].innerText = classes[0][2].room;
+        } else {
+            e.children[0].innerText = "Free";
+        }
+    });
+
+    Array.from(document.getElementsByClassName('line4')).forEach(e => {
+        if(classes[0][3]) {
+            e.children[0].innerText = classes[0][3].subject;
+            e.children[1].innerText = classes[0][3].teacher;
+            e.children[2].innerText = classes[0][3].room;
+        } else {
+            e.children[0].innerText = "Free";
+        }
+    });
+
+    Array.from(document.getElementsByClassName('line5')).forEach(e => {
+        if(classes[0][4]) {
+            e.children[0].innerText = classes[0][4].subject;
+            e.children[1].innerText = classes[0][4].teacher;
+            e.children[2].innerText = classes[0][4].room;
+        } else {
+            e.children[0].innerText = "Free";
+        }
+    });
+
+    Array.from(document.getElementsByClassName('line6')).forEach(e => {
+        if(classes[1][0]) {
+            e.children[0].innerText = classes[1][0].subject;
+            e.children[1].innerText = classes[1][0].teacher;
+            e.children[2].innerText = classes[1][0].room;
+        } else {
+            e.children[0].innerText = "Free";
+        }
+    });
+
+    Array.from(document.getElementsByClassName('line7')).forEach(e => {
+        if(classes[1][1]) {
+            e.children[0].innerText = classes[1][1].subject;
+            e.children[1].innerText = classes[1][1].teacher;
+            e.children[2].innerText = classes[1][1].room;
+        } else {
+            e.children[0].innerText = "Free";
+        }
+    });
+
+    Array.from(document.getElementsByClassName('mentorclass')).forEach(e => {
+        if(e.childElementCount) {
+            if(classes[1][2]) {
+                e.children[0].innerText = classes[1][2].subject;
+                e.children[1].innerText = classes[1][2].teacher;
+                e.children[2].innerText = classes[1][2].room;
+            } else {
+                e.children[0].innerText = "Free";
+            }
+        }
     });
 
     // there has got to be a more efficient way to do this 
-    // also, will throw error if person doen't have a lesson then (free line)
 }
